b40a1b97911360a41f1812e5f75906c1da3a8ad5
jae <wogud1514@naver.com>
2022-09-30 17:49:31 +0900

    feat : 개인 운행이력 조회 리포지토리 테스트까지

--- src/main/java/jjfactory/simpleapi/business/delivery/repository/DeliveryRepositorySupport.java
+++ src/main/java/jjfactory/simpleapi/business/delivery/repository/DeliveryRepositorySupport.java
@@ -7,8 +7,13 @@ import jjfactory.simpleapi.business.accident.domain.Accident;
 import jjfactory.simpleapi.business.delivery.domain.QDelivery;
 import jjfactory.simpleapi.business.delivery.dto.DeliveryRes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static jjfactory.simpleapi.business.delivery.domain.QDelivery.*;
@@ -25,5 +30,30 @@ public class DeliveryRepositorySupport {
                 .from(delivery)
                 .where(delivery.appointTime.before(accident.getAccidentTime()))
                 .fetch();
+    }
+
+    public Page<DeliveryRes> findMyDeliveries(Pageable pageable,String startDate, String endDate, Long riderId){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime convertedStart = LocalDateTime.parse(startDate, formatter);
+        LocalDateTime convertedEnd = LocalDateTime.parse(endDate, formatter);
+
+        List<DeliveryRes> deliveries = queryFactory.select(Projections.constructor(DeliveryRes.class, delivery))
+                .from(delivery)
+                .where(delivery.appointTime.between(convertedStart, convertedEnd),
+                        delivery.id.isNotNull(),
+                        delivery.rider.id.eq(riderId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(delivery.appointTime.desc())
+                .fetch();
+
+        int count = queryFactory.select(Projections.constructor(DeliveryRes.class, delivery))
+                .from(delivery)
+                .where(delivery.appointTime.between(convertedStart, convertedEnd),
+                        delivery.id.isNotNull(),
+                        delivery.rider.id.eq(riderId))
+                .fetch().size();
+
+        return new PageImpl<>(deliveries,pageable,count);
     }
 }
---
+++ src/test/java/jjfactory/simpleapi/business/delivery/repository/DeliveryRepositorySupportTest.java
@@ -0,0 +1,113 @@
+package jjfactory.simpleapi.business.delivery.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jjfactory.simpleapi.business.accident.domain.Accident;
+import jjfactory.simpleapi.business.delivery.domain.Address;
+import jjfactory.simpleapi.business.delivery.domain.Delivery;
+import jjfactory.simpleapi.business.delivery.dto.DeliveryRes;
+import jjfactory.simpleapi.business.rider.domain.Rider;
+import jjfactory.simpleapi.business.seller.domain.Seller;
+import jjfactory.simpleapi.global.dto.req.MyPageReq;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+
+import javax.persistence.EntityManager;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static jjfactory.simpleapi.business.delivery.domain.QDelivery.delivery;
+import static org.assertj.core.api.Assertions.*;
+
+
+@DataJpaTest
+class DeliveryRepositorySupportTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+    Long idTmp = 0L;
+
+    @BeforeEach
+    public void init() {
+        queryFactory = new JPAQueryFactory(em);
+        Rider rider = createRiderAndSeller();
+        idTmp = rider.getId();
+        createDeliveries(rider);
+    }
+
+    @Test
+    @DisplayName("라이더 운행 아력조회 성공")
+    void findMyDeliveries() {
+        //given
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime convertedStart = LocalDateTime.parse("2022-08-01 00:00:00", formatter);
+        LocalDateTime convertedEnd = LocalDateTime.parse("2022-09-30 23:59:59", formatter);
+
+        PageRequest req = new MyPageReq(1, 10).of();
+
+        //when
+        List<DeliveryRes> deliveries = queryFactory.select(Projections.constructor(DeliveryRes.class, delivery))
+                .from(delivery)
+                .where(delivery.appointTime.between(convertedStart, convertedEnd),
+                        delivery.id.isNotNull(),
+                        delivery.rider.id.eq(idTmp))
+                .offset(req.getOffset())
+                .limit(req.getPageSize())
+                .orderBy(delivery.appointTime.desc())
+                .fetch();
+
+        int count = queryFactory.select(Projections.constructor(DeliveryRes.class, delivery))
+                .from(delivery)
+                .where(delivery.appointTime.between(convertedStart, convertedEnd),
+                        delivery.id.isNotNull(),
+                        delivery.rider.id.eq(idTmp))
+                .fetch().size();
+
+        //then
+        assertThat(count).isEqualTo(7);
+        assertThat(LocalDateTime.parse(deliveries.get(0).getAppointTime(),formatter))
+                .isAfter(LocalDateTime.parse(deliveries.get(6).getAppointTime(),formatter));
+    }
+
+    private Rider createRiderAndSeller() {
+        Rider rider = Rider.builder()
+                .driverId("GG1234")
+                .name("tester")
+                .password("1234")
+                .phone("01012341234")
+                .build();
+
+        em.persist(rider);
+
+        Seller findSeller = Seller.builder()
+                .name("sellerA")
+                .sellerCode("dddd1111wwww")
+                .build();
+        em.persist(findSeller);
+        return rider;
+    }
+
+    private void createDeliveries(Rider rider) {
+        for(int i = 1; i<16; i++){
+            LocalDateTime appointTime = ( i%2 == 0 ) ?  LocalDateTime.now() : LocalDateTime.now().minusMonths(3);
+
+            Delivery delivery = Delivery.builder()
+                    .rider(rider)
+                    .appointTime(appointTime.plusMinutes(i))
+                    .address(new Address("주소1","주소2","배달주소1","배달주소2"))
+                    .build();
+
+            em.persist(delivery);
+
+        }
+    }
+}
